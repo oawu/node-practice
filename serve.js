7595fbeb195e8c71d5c578e609f01c7428fae0f9
OA Wu <comdan66@gmail.com>
2020-07-25 13:20:31 +0800

    server

--- serve.js
+++ serve.js
@@ -5,5 +5,10 @@ http.on('error', error => console.error('錯誤', error))
 http.listen(port, _ => {
   console.error('機器開好了，網址是：http://127.0.0.1:' + port + '/');
 })
+http.on('request', (request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
+  response.write('Hi, Hello World!')
+  response.end()
+})
 
 
