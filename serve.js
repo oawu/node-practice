1043c0f8146ff63f722b89d22c3c5dbc0880c5bd
OA Wu <comdan66@gmail.com>
2020-07-25 13:15:18 +0800

    init

--- serve.js
+++ serve.js
@@ -1,1 +1,9 @@
-console.error(111111);
+const port = 8032
+
+const http = require('http').Server()
+http.on('error', error => console.error('錯誤', error))
+http.listen(port, _ => {
+  console.error('機器開好了，網址是：http://127.0.0.1:' + port + '/');
+})
+
+
