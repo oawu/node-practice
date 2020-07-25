4572cd2e56c69d53f3564b6ff30ee3d9dfa60cae
OA Wu <comdan66@gmail.com>
2020-07-25 13:28:43 +0800

    url parse

--- serve.js
+++ serve.js
@@ -13,14 +13,45 @@ http.on('request', (request, response) => {
   const pathname = url.pathname.replace(/\/+/gm, '/').replace(/\/$|^\//gm, '')
 
   // 比對 Router
-  // Router.mapping({ method, pathname, request, response }, {
-  // //   gets: url.query,
-  // //   raw: Buffer.concat(param).toString()
-  // })
+  Router.mapping({ method, pathname, request, response })
 
-  response.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
-  response.write('Hi, Hello World!')
-  response.end()
 })
 
+const Router = {
+  mapping ({ method, pathname, request, response }) {
 
+console.error(pathname);
+
+
+
+    response.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
+    response.write('Hi, Hello World!')
+    response.end()
+    // pathname = pathname === '' ? 'index' : pathname
+
+  //   const dirs = pathname.split('/')
+  //   const file = dirs.pop()
+
+  //   params.gets = getGET(params.gets)
+
+  //   try { params.json = JSON.parse(params.raw) }
+  //   catch (e) { params.json = null }
+
+  //   const api = Path.api + (dirs.length ? dirs.join(Path.sep) + Path.sep : '') + method + '-' + file + '.js'
+  //   const notFound = Path.api + this.notFound + '.js'
+
+  //   return FileSystem.promises.access(api, FileSystem.constants.R_OK)
+  //     .then(_ => {
+  //       if (Config.val.env != 'Production')
+  //         delete require.cache[api]
+  //       require(api)(request, response, params, pathname)
+  //     })
+  //     .catch(e => FileSystem.promises.access(notFound, FileSystem.constants.R_OK)
+  //       .then(_ => {
+  //         if (Config.val.env != 'Production')
+  //           delete require.cache[notFound]
+  //         require(notFound)(request, response, params, pathname, e.message)
+  //       })
+  //       .catch(_ => this.error(request, response, params, pathname, e.message)))
+  // }
+}
