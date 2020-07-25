7924a759e331703dd39bad89efe18f206cb28e4f
OA Wu <comdan66@gmail.com>
2020-07-25 13:24:12 +0800

    url parse

--- serve.js
+++ serve.js
@@ -6,10 +6,17 @@ http.listen(port, _ => {
   console.error('機器開好了，網址是：http://127.0.0.1:' + port + '/');
 })
 http.on('request', (request, response) => {
-    const URL = require('url')
-    const url = URL.parse(request.url)
-    console.error(url.pathname)
-    // const pathname = url.pathname.replace(/\/+/gm, '/').replace(/\/$|^\//gm, '')
+  const URL = require('url')
+  
+  const url = URL.parse(request.url)
+  const method = request.method.toUpperCase()
+  const pathname = url.pathname.replace(/\/+/gm, '/').replace(/\/$|^\//gm, '')
+
+  // 比對 Router
+  // Router.mapping({ method, pathname, request, response }, {
+  // //   gets: url.query,
+  // //   raw: Buffer.concat(param).toString()
+  // })
 
   response.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'})
   response.write('Hi, Hello World!')
